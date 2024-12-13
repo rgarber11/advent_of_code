99e9b29c15d16fd357bc529216a233c0ce07c831
Richard Garber <rg.1029384756@gmail.com>
2024-12-13 12:35:27 -0500

    Initial Boilerplate

---
+++ 2024/day13/src/lib.rs
@@ -0,0 +1,28 @@
+use std::env::Args;
+pub struct Settings {
+    pub is_part_two: bool,
+    pub file_path: Box<str>,
+}
+impl Settings {
+    pub fn parse_args(mut args: Args) -> Option<Settings> {
+        match args.len() {
+            1 => Some(Settings {
+                is_part_two: false,
+                file_path: Box::from("/home/rgarber11/advent_of_code/2024/day13/input"),
+            }),
+            2 => Some(Settings {
+                is_part_two: false,
+                file_path: unsafe { Box::from(args.nth(1).unwrap_unchecked()) },
+            }),
+            3 => Some(Settings {
+                is_part_two: match unsafe { args.nth(1).unwrap_unchecked().as_str() } {
+                    "1" => false,
+                    "2" => true,
+                    _ => panic!("Only parts 1 or 2 are options"),
+                },
+                file_path: unsafe { Box::from(args.nth(0).unwrap_unchecked()) },
+            }),
+            _ => None,
+        }
+    }
+}
--- 2024/day13/src/main.rs
+++ 2024/day13/src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
+use day13::{FileData, Settings};
+
 fn main() {
-    println!("Hello, world!");
+    let settings = Settings::parse_args(env::args()).expect("Couldn't parse args.");
+    let file_data = FileData::read_file(&settings.file_path).expect("Couldn't read file data.");
+    let ans = if settings.is_part_two {
+        day13::part2(file_data)
+    } else {
+        day13::part1(file_data)
+    };
+    println!("The answer is {}", ans);
 }
